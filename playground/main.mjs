10e732d274703f8adcdd9836cc45c1857383611f
nasuda <nasuda908@gmail.com>
2026-05-20 16:06:41 +0900

    add input function

--- playground/main.mjs
+++ playground/main.mjs
@@ -1,7 +1,11 @@
-const args = process.argv.slice(2);
-console.log(args);
+function input() {
+
+  const args = process.argv.slice(2);
+  console.log(args);
+}
 
 function main() {
+  input();
   console.log("main");
 }
 
