7bd6961257d8f9f7b9300cd9a877bb4584a06ad7
nasuda <nasuda908@gmail.com>
2026-05-31 04:12:03 +0900

    add writ

--- playground/memo.mjs
+++ playground/memo.mjs
@@ -2,5 +2,9 @@ import { writeFileSync } from 'node:fs';
 console.log(writeFileSync);
 
 const memoContent = process.argv.slice(2);
+const memo = memoContent[0];
 
 console.log(memoContent);
+
+
+writeFileSync("./out/memo.md", memo);
