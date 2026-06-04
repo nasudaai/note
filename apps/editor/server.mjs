4166c8cc1031b34674a5fb5e1ae3f435f794d155
nasuda <nasuda908@gmail.com>
2026-06-04 16:09:10 +0900

    add server

--- apps/editor/server.mjs
+++ apps/editor/server.mjs
@@ -1,3 +1,12 @@
 import { createServer } from 'node:http';
 
 console.log(createServer);
+
+const server = createServer((req, res) => {
+  res.end("hello");
+});
+
+
+server.listen(3000, () => {
+  console.log("listen http://localhost:3000");
+});
