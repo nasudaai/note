4b5a128a01132c7002d95ab1132be6db632b9ba8
nasuda <nasuda908@gmail.com>
2026-06-22 09:55:15 +0900

    add types per of second

--- apps/yen/main.mjs
+++ apps/yen/main.mjs
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2);
-console.log(args);
-console.log("yen")
 
 const obj = {
   yen: args[0],
-  correct: args[1]
+  correct: args[1],
+  typesPerOfSecond: args[2],
 };
 
 console.log(obj.yen);
+console.log(obj.typesPerOfSecond);
