f235db7068af0588813ab83e1b6423677c86c054
nasuda <nasuda908@gmail.com>
2026-07-11 20:09:20 +0900

    comment out

--- apps/app/main.mjs
+++ apps/app/main.mjs
@@ -1,9 +1,10 @@
 const args = process.argv.slice(2);
 console.log(args);
-console.log("main");
+//console.log("main");
 
 function main () {
-  console.log("main");
+//  console.log("main");
+  console.log(args);
 };
 
 main();
