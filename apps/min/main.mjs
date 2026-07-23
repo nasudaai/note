46eb6999c5f6c49723202fd0b92634055ac4d689
nasuda <nasuda908@gmail.com>
2026-07-23 09:13:02 +0900

    add json

--- apps/min/main.mjs
+++ apps/min/main.mjs
@@ -6,6 +6,9 @@ const obj = {};
 function main() {
 
   const args = process.argv.slice(2);
+  const inputData = JSON.stringify(args);
+  console.log(inputData);
+
   console.log(args);
   console.log("main");
 };
